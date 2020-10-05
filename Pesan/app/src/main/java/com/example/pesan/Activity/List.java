3529bfcdc49e80b86a3e3a5d836955515a3ab0a4
ima28 <zumaroimaa@gmail.com>
2020-10-05 15:41:35 +0700

    05/10

---
+++ Pesan/app/src/main/java/com/example/pesan/Activity/List.java
@@ -0,0 +1,15 @@
+package com.example.pesan.Activity;
+
+import com.example.pesan.R;
+
+class List {
+    static int[] iconList = {
+            R.drawable.wall_1, R.drawable.wall_2
+    };
+    static String[] Headline = {"wonu", " wwww"
+    };
+
+    static String[] Subhead = {"2019", "2020"
+
+    };
+}
--- Pesan/app/src/main/java/com/example/pesan/Activity/Produk.java
+++ Pesan/app/src/main/java/com/example/pesan/Activity/Produk.java
@@ -1,16 +1,43 @@
 package com.example.pesan.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.pesan.Adapter.AdapterRecyclerView;
 import com.example.pesan.R;
 
+import java.util.ArrayList;
+
 public class Produk extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.Adapter recyclerViewAdapter;
+    RecyclerView.LayoutManager recyclerViewLayoutManager;
+    ArrayList<ItemModel> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produk);
+
+        recyclerView = findViewById(R.id.re);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerViewLayoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+
+        data = new ArrayList<>();
+        for (int i = 0; i < List.Headline.length; i++) {
+            data.add(new ItemModel(
+            List.Headline[i],
+                    List.Subhead[i],
+                    List.iconList[i]
+            ));
+        }
+        recyclerViewAdapter = new AdapterRecyclerView(data);
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 }
